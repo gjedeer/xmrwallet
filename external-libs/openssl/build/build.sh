12d48d44d6536fe34956a28ff186b546a13034e5
GDR! <gdr@gdr.name>
2017-12-28 14:19:14 +0000

    Make depend in openssl build

--- external-libs/openssl/build/build.sh
+++ external-libs/openssl/build/build.sh
@@ -18,6 +18,7 @@ tar xfz openssl-$OPENSSL_VERSION.tar.gz
 cd openssl-$OPENSSL_VERSION/
 perl -pi -e 's/install: all install_docs install_sw/install: install_docs install_sw/g' Makefile.org
 CROSS_COMPILE= CC="$ndkdir/bin/arm-linux-androideabi-gcc" ./Configure shared no-ssl2 no-ssl3 no-comp no-hw no-engine android-armv7 --openssldir=$OPENSSL_BUILD_ROOT
+make depend
 make build_libs
 
 cp -v libcrypto.a "$BUILD_ROOT/../external-libs/openssl/lib/armeabi-v7a/"
